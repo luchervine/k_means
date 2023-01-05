636a739e02ee092235195a5661beecbbcb197f45
luchervine <luchervine@gmail.com>
2023-01-05 08:55:45 +0100

    update anova

--- src/anova/Application.java
+++ src/anova/Application.java
@@ -11,7 +11,10 @@ public class Application {
         data.add(new int[]{16,18,11});
         data.add(new int[]{23,31,24});
 
-        Risque risque = new Risque(data);
+        data1.add(new int[]{4,5,10,4,3,11,14,6,10,12});
+        data1.add(new int[]{21,19,24,17,16,25,24,22,21,21});
+
+        Risque risque = new Risque(data1);
         risque.calcul_sceT();
         risque.calcul_sceA();
         risque.calcul_sceR();
